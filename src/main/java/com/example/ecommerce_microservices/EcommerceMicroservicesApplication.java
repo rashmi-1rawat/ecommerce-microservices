5101103acf1f4ec8ac23c0858a730ea2182645bc
Rashmi <rashmirwt5@gmail.com>
2025-02-14 09:21:42 +0530

    Initial commit - Sync local project with GitHub

---
+++ src/main/java/com/example/ecommerce_microservices/EcommerceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMicroservicesApplication.class, args);
+	}
+
+}
